3a0e6a47458be7af23769ef25fc83eed29b66a67
Vladimir <vtischenko@meta.ua>
2016-12-25 14:57:31 +0200

    HW03 - repairs SpringMain / InMemoryAdminRestControllerTest / InMemoryAdminRestControllerSpringTest

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
@@ -10,6 +12,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -19,18 +22,19 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext(
+                "spring/spring-app.xml", "classpath:spring/spring-db.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(UserTestData.USER);
+            adminUserController.create(new User(null, "NewSpringMainUser", "new@gmail.com", "newPass", 1555, false, Collections.singleton(Role.ROLE_USER)));
             System.out.println();
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealWithExceed> filteredMealsWithExceeded =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);
+//            MealRestController mealController = appCtx.getBean(MealRestController.class);
+//            List<MealWithExceed> filteredMealsWithExceeded =
+//                    mealController.getBetween(
+//                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+//                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+//            filteredMealsWithExceeded.forEach(System.out::println);
         }
     }
 }
--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
@@ -1,48 +1,41 @@
 package ru.javawebinar.topjava.web;
 
 import org.junit.*;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.UserTestData;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.DbPopulator;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
-import java.util.Arrays;
 import java.util.Collection;
 
-import static ru.javawebinar.topjava.UserTestData.ADMIN;
-import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.*;
 
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
 public class InMemoryAdminRestControllerTest {
-    private static ConfigurableApplicationContext appCtx;
-    private static AdminRestController controller;
 
-    @BeforeClass
-    public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
-        controller = appCtx.getBean(AdminRestController.class);
-    }
+    @Autowired
+    private AdminRestController controller;
 
-    @AfterClass
-    public static void afterClass() {
-        appCtx.close();
-    }
+    @Autowired
+    private DbPopulator dbPopulator;
 
     @Before
     public void setUp() throws Exception {
         // Re-initialize
-        UserRepository repository = appCtx.getBean(UserRepository.class);
-        repository.getAll().forEach(u -> repository.delete(u.getId()));
-        repository.save(USER);
-        repository.save(ADMIN);
+        dbPopulator.execute();
     }
 
     @Test
     public void testDelete() throws Exception {
-        controller.delete(UserTestData.USER_ID);
+        controller.delete(USER_ID);
         Collection<User> users = controller.getAll();
         Assert.assertEquals(users.size(), 1);
         Assert.assertEquals(users.iterator().next(), ADMIN);
