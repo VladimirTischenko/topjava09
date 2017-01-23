fb9675c3a563d4e06bec33548f970b653e564d51
Vladimir <vtischenko@meta.ua>
2017-01-23 21:15:58 +0200

    HW06 - Jdbc tests are repaired

---
+++ src/main/java/ru/javawebinar/topjava/repository/JdbcUtil.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.repository;
+
+/**
+ * Created by Vladimir on 23.01.2017.
+ */
+public class JdbcUtil {
+
+    public void clear2ndLevelHibernateCache() {
+    }
+}
--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,10 +1,23 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.repository.JdbcUtil;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+
+    @Autowired
+    private JdbcUtil jdbcUtil;
+
+    @Before
+    public void setUp() throws Exception {
+        service.evictCache();
+        jdbcUtil.clear2ndLevelHibernateCache();
+    }
+
 }
