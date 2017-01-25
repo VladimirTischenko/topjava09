7278b9056f6fd218ef950f1f3f47d65ff90d0bec
Vladimir <vtischenko@meta.ua>
2017-01-25 09:32:49 +0200

    HW06 - functionality of MealServlet move to controll

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -6,9 +6,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 /**
  * User: gkislin
@@ -17,7 +32,12 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class RootController {
     @Autowired
-    private UserService service;
+    private UserService userService;
+
+    @Autowired
+    private MealService mealService;
+
+    private int userId = AuthorizedUser.id();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
@@ -26,7 +46,7 @@ public class RootController {
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public String users(Model model) {
-        model.addAttribute("users", service.getAll());
+        model.addAttribute("users", userService.getAll());
         return "users";
     }
 
@@ -35,5 +55,71 @@ public class RootController {
         int userId = Integer.valueOf(request.getParameter("userId"));
         AuthorizedUser.setId(userId);
         return "redirect:meals";
+    }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.GET)
+    public String meals(Model model, HttpServletRequest request) {
+        model.addAttribute("meals", MealsUtil.getWithExceeded(mealService.getAll(userId),
+                AuthorizedUser.getCaloriesPerDay()));
+        return "meals";
+    }
+
+    @RequestMapping(value = "/deleteMeal", method = RequestMethod.GET)
+    public String deleteMeal(HttpServletRequest request) {
+        int id = getId(request);
+        mealService.delete(id, userId);
+        return "redirect:meals";
+    }
+
+    @RequestMapping(value = "/saveMeal", method = RequestMethod.GET)
+    public String saveMeal(Model model, HttpServletRequest request) {
+        final Meal meal = request.getParameter("id") == null ?
+                new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+                mealService.get(getId(request), userId);
+        model.addAttribute("meal", meal);
+        return "meal";    }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.POST)
+    public String save(HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+
+        final Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.valueOf(request.getParameter("calories")));
+
+        if (request.getParameter("id").isEmpty()) {
+            checkNew(meal);
+            mealService.save(meal, userId);
+        } else {
+            checkIdConsistent(meal, getId(request));
+            mealService.update(meal, userId);
+        }
+        return "redirect:meals";
+    }
+
+    @RequestMapping(value = "/filter", method = RequestMethod.POST)
+    public String filter(HttpServletRequest request) throws UnsupportedEncodingException {
+        LocalDate startDate = DateTimeUtil.parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = DateTimeUtil.parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = DateTimeUtil.parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = DateTimeUtil.parseLocalTime(request.getParameter("endTime"));
+
+        List<MealWithExceed> filteredWithExceeded = MealsUtil.getFilteredWithExceeded(
+                mealService.getBetweenDates(
+                        startDate != null ? startDate : DateTimeUtil.MIN_DATE,
+                        endDate != null ? endDate : DateTimeUtil.MAX_DATE, userId),
+                startTime != null ? startTime : LocalTime.MIN,
+                endTime != null ? endTime : LocalTime.MAX,
+                AuthorizedUser.getCaloriesPerDay()
+        );
+
+        request.setAttribute("meals", filteredWithExceeded);
+        return "meals";
+    }
+
+        private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.valueOf(paramId);
     }
 }
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -3,14 +3,11 @@
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="fn" uri="http://topjava.javawebinar.ru/functions" %>
 <html>
-<head>
-    <title>Meal list</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<jsp:include page="fragments/headTag.jsp"/>
 <body>
 <section>
     <h3>Meal list</h3>
-    <form method="post" action="meals?action=filter">
+    <form method="post" action="filter">
         <dl>
             <dt>From Date:</dt>
             <dd><input type="date" name="startDate" value="${param.startDate}"></dd>
@@ -30,7 +27,7 @@
         <button type="submit">Filter</button>
     </form>
     <hr>
-    <a href="meals?action=create">Add Meal</a>
+    <a href="saveMeal">Add Meal</a>
     <hr>
     <table border="1" cellpadding="8" cellspacing="0">
         <thead>
@@ -52,8 +49,8 @@
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
-                <td><a href="meals?action=update&id=${meal.id}">Update</a></td>
-                <td><a href="meals?action=delete&id=${meal.id}">Delete</a></td>
+                <td><a href="saveMeal?id=${meal.id}">Update</a></td>
+                <td><a href="deleteMeal?id=${meal.id}">Delete</a></td>
             </tr>
         </c:forEach>
     </table>
